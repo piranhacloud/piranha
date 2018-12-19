eecef52659008dc0ab0ba8fa6840d477a5fbb67e
Manfred Riem <mriem@manorrock.com>
2018-12-19 05:56:36 +0100

    Fixes issue #1 - Implement Netty integration

---
+++ piranha-netty/src/test/java/com/manorrock/piranha/netty/NettyHttpServerTest.java
@@ -0,0 +1,122 @@
+/*
+ *  Copyright (c) 2002-2018, Manorrock.com. All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *      1. Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *
+ *      2. Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.piranha.netty;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * The JUnit tests for the DefaultHttpServer class.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class NettyHttpServerTest {
+
+    /**
+     * Test start and stop method.
+     */
+    @Test
+    public void testStartAndStop() {
+        NettyHttpServer server = new NettyHttpServer(28001);
+        server.start();
+        assertTrue(server.isRunning());
+        server.stop();
+        assertFalse(server.isRunning());
+    }
+
+    /**
+     * Test processing.
+     */
+    @Test
+    public void testProcessing() {
+        NettyHttpServer server = new NettyHttpServer(28002);
+        server.start();
+        try {
+            HttpClient client = HttpClients.createDefault();
+            HttpGet request = new HttpGet("http://localhost:28002");
+            HttpResponse response = client.execute(request);
+            assertEquals(200, response.getStatusLine().getStatusCode());
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+        server.stop();
+        assertFalse(server.isRunning());
+    }
+
+    /**
+     * Test file not found.
+     */
+    @Test
+    public void testFileNotFound() {
+        NettyHttpServer server = new NettyHttpServer(28003);
+        server.start();
+        try {
+            HttpClient client = HttpClients.createDefault();
+            HttpGet request = new HttpGet("http://localhost:28003/this_is_certainly_not_there");
+            HttpResponse response = client.execute(request);
+            assertEquals(404, response.getStatusLine().getStatusCode());
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+        server.stop();
+        assertFalse(server.isRunning());
+    }
+
+    /**
+     * Test file.
+     * 
+     * @throws Exception when a serious error occurs.
+     */
+    @Test
+    public void testFile() throws Exception {
+        NettyHttpServer server = new NettyHttpServer(28004);
+        server.start();
+        try {
+            HttpClient client = HttpClients.createDefault();
+            HttpGet request = new HttpGet("http://localhost:28004/pom.xml");
+            HttpResponse response = client.execute(request);
+            assertEquals(200, response.getStatusLine().getStatusCode());
+            HttpEntity entity = response.getEntity();
+            ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+            entity.writeTo(byteOutput);
+            String responseText = byteOutput.toString("UTF-8");
+            assertTrue(responseText.contains("modelVersion"));
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+        server.stop();
+        assertFalse(server.isRunning());
+    }
+}
