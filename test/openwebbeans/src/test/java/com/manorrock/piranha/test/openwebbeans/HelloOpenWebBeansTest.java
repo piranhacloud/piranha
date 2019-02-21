4e1a2da8fefedbd98d787fd2074a6ca0d837256b
Manfred Riem <mriem@manorrock.com>
2019-02-20 21:51:03 -0700

    Fixes issue #31 - Add OpenWebBeans integration module

---
+++ test/openwebbeans/src/test/java/com/manorrock/piranha/test/openwebbeans/HelloOpenWebBeansTest.java
@@ -0,0 +1,75 @@
+/*
+ *  Copyright (c) 2002-2018, Manorrock.com. All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *      1. Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *
+ *      2. Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.piranha.test.openwebbeans;
+
+import com.manorrock.piranha.DefaultAliasedDirectoryResource;
+import com.manorrock.piranha.DefaultDirectoryResource;
+import com.manorrock.piranha.DefaultWebApplication;
+import com.manorrock.piranha.test.utils.TestHttpServletRequest;
+import com.manorrock.piranha.test.utils.TestHttpServletResponse;
+import com.manorrock.piranha.test.utils.TestServletOutputStream;
+import java.io.File;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * The JUnit tests for the Hello Weld web application.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class HelloOpenWebBeansTest {
+
+    /**
+     * Test /index.html.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testIndexHtml() throws Exception {
+        System.getProperties().put("java.naming.factory.initial", "com.manorrock.herring.DefaultInitialContextFactory");
+        DefaultWebApplication webApp = new DefaultWebApplication();
+        webApp.addResource(new DefaultDirectoryResource(new File("src/main/webapp")));
+        webApp.addResource(new DefaultAliasedDirectoryResource(new File("target/classes"), "/WEB-INF/classes"));
+        webApp.addInitializer("com.manorrock.piranha.openwebbeans.OpenWebBeansInitializer");
+        webApp.addInitializer("com.manorrock.piranha.mojarra.MojarraInitializer");
+        webApp.initialize();
+        webApp.start();
+        TestHttpServletRequest request = new TestHttpServletRequest();
+        request.setWebApplication(webApp);
+        request.setContextPath("");
+        request.setServletPath("/index.html");
+        request.setPathInfo(null);
+        TestHttpServletResponse response = new TestHttpServletResponse();
+        TestServletOutputStream outputStream = new TestServletOutputStream();
+        response.setOutputStream(outputStream);
+        outputStream.setResponse(response);
+        webApp.service(request, response);
+        assertEquals(200, response.getStatus());
+        String responseString = new String(response.getResponseBody());
+        assertTrue(responseString.contains("Hello OpenWebBeans"));
+    }
+}
