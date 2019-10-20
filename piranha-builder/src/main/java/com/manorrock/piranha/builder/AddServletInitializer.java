711dfb6c4b5659598008296449b9baaecfdb817f
arjantijms <arjan.tijms@gmail.com>
2019-10-21 00:26:51 +0200

    #215 Add initializer for Servlet to prevent them init b4 initializers
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

---
+++ piranha-builder/src/main/java/com/manorrock/piranha/builder/AddServletInitializer.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2002-2019 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its 
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.piranha.builder;
+
+import java.util.Set;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import com.manorrock.piranha.api.WebApplication;
+
+/**
+ * Initializer that install a Servlet
+ * 
+ * @author Arjan Tijms
+ */
+public class AddServletInitializer implements ServletContainerInitializer {
+    
+    private final Class<? extends Servlet> servletClass;
+    private final String[] urlPatterns;
+    
+    public AddServletInitializer(Class<? extends Servlet> servletClass, String[] urlPatterns) {
+        this.servletClass = servletClass;
+        this.urlPatterns = urlPatterns;
+    }
+
+   
+    @Override
+    public void onStartup(Set<Class<?>> classes, ServletContext servletContext) throws ServletException {
+        servletContext.addServlet(servletClass.getSimpleName(), servletClass);
+        
+        WebApplication context = (WebApplication) servletContext;
+
+        // TMP - should use Dynamic
+        context.addServletMapping(servletClass.getSimpleName(), urlPatterns);
+    }
+
+}
