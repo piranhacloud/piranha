ba3719159ba18cd870a15f478d4c1484cf1775ed
arjantijms <arjan.tijms@gmail.com>
2019-10-27 16:44:15 +0100

    #259 Add support for ShrinkWrap resources
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

---
+++ piranha-resources-shrinkwrap/src/main/java/com/manorrock/piranha/shrinkwrap/ArchiveURLStreamHandler.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2002-2019 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its 
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.piranha.shrinkwrap;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLStreamHandler;
+
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.Node;
+import org.jboss.shrinkwrap.api.asset.Asset;
+
+/**
+ * 
+ * @author Arjan Tijms
+ *
+ */
+public class ArchiveURLStreamHandler extends URLStreamHandler {
+    
+    private Archive<?> archive;
+    
+    public ArchiveURLStreamHandler(Archive<?> archive) {
+        this.archive = archive;
+    }
+
+    @Override
+    protected StreamConnection openConnection(URL requestedUrl) throws IOException {
+        return new StreamConnection(requestedUrl) {
+
+            @Override
+            public InputStream getInputStream() throws IOException {
+                Node node = getNode();
+                if (node == null) {
+                    throw new IllegalStateException("Can't resolve URL " + requestedUrl.toExternalForm());
+                }
+
+                Asset asset = node.getAsset();
+                if (asset == null) {
+                    return null;
+                }
+                
+                return asset.openStream();
+            }
+            
+            private Node getNode() {
+                return archive.get(
+                    ArchivePaths.create(
+                        requestedUrl
+                            .getPath()
+                            .replace(archive.getName(), "")));
+            }
+        };
+        
+        
+    }
+    
+
+    
+}
