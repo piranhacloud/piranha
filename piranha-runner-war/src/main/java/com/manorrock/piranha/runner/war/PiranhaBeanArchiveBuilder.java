2718dcf4c3242da209d31e483dcd722cb2282090
arjantijms <arjan.tijms@gmail.com>
2019-11-14 17:12:14 +0100

    #253 Updated Arquillian connector with isolation support
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- piranha-runner-war/src/main/java/com/manorrock/piranha/runner/war/PiranhaBeanArchiveBuilder.java
+++
@@ -1,7 +0,0 @@
-package com.manorrock.piranha.runner.war;
-
-import org.jboss.weld.environment.deployment.discovery.BeanArchiveBuilder;
-
-public class PiranhaBeanArchiveBuilder extends BeanArchiveBuilder {
-
-}
