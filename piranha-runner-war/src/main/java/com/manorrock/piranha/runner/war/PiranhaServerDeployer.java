269cfe2d198181e9f3533078a208ddbd0418930f
arjantijms <arjan.tijms@gmail.com>
2019-11-17 00:20:09 +0100

    #355 Take servlet annotations from JANDEX.
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- piranha-runner-war/src/main/java/com/manorrock/piranha/runner/war/PiranhaServerDeployer.java
+++ piranha-runner-war/src/main/java/com/manorrock/piranha/runner/war/PiranhaServerDeployer.java
@@ -27,14 +27,33 @@ package com.manorrock.piranha.runner.war;
 
 import static com.manorrock.piranha.authorization.exousia.AuthorizationPreInitializer.AUTHZ_FACTORY_CLASS;
 import static com.manorrock.piranha.authorization.exousia.AuthorizationPreInitializer.AUTHZ_POLICY_CLASS;
+import static java.util.Arrays.stream;
 import static javax.naming.Context.INITIAL_CONTEXT_FACTORY;
+import static org.jboss.jandex.DotName.createSimple;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.annotation.WebServlet;
+
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.Index;
+import org.jboss.jandex.IndexReader;
 import org.jboss.shrinkwrap.api.Archive;
 import org.omnifaces.exousia.modules.def.DefaultPolicy;
 import org.omnifaces.exousia.modules.def.DefaultPolicyConfigurationFactory;
 
+import com.manorrock.piranha.DefaultAnnotationManager;
+import com.manorrock.piranha.DefaultAnnotationManager.DefaultAnnotationInfo;
 import com.manorrock.piranha.DefaultHttpServer;
 import com.manorrock.piranha.DefaultWebApplication;
 import com.manorrock.piranha.DefaultWebApplicationServer;
@@ -46,7 +65,9 @@ import com.manorrock.piranha.authorization.exousia.AuthorizationPreInitializer;
 import com.manorrock.piranha.security.jakarta.JakartaSecurityInitializer;
 import com.manorrock.piranha.security.soteria.SoteriaInitializer;
 import com.manorrock.piranha.servlet.ServletFeature;
+import com.manorrock.piranha.servlet.WebAnnotationInitializer;
 import com.manorrock.piranha.shrinkwrap.ShrinkWrapResource;
+import com.manorrock.piranha.webxml.WebXmlInitializer;
 import com.manorrock.piranha.weld.WeldInitializer;
 
 /**
@@ -57,6 +78,14 @@ import com.manorrock.piranha.weld.WeldInitializer;
  */
 public class PiranhaServerDeployer {
     
+    Class<?>[] webAnnotations = new Class<?>[] {
+       WebServlet.class, 
+       WebListener.class,
+       WebInitParam.class,
+       WebFilter.class,
+       ServletSecurity.class,
+       MultipartConfig.class};
+    
     private HttpServer httpServer;
     
     public Set<String> start(Archive<?> applicationArchive, ClassLoader classLoader) {
@@ -65,9 +94,35 @@ public class PiranhaServerDeployer {
         
         WebApplication webApplication = getWebApplication(applicationArchive, classLoader);
         
+        // Source of annotations
+        Index index = getIndex();
+        
+        // Target of annotations
+        DefaultAnnotationManager annotationManager = (DefaultAnnotationManager) webApplication.getAnnotationManager();
+        
+        // Copy from source index to target manager
+        forEachWebAnnotation(webAnnotation ->
+            // Read the web annotations (@WebServlet.class etc) from the source index
+            getAnnotations(index, webAnnotation)
+                // Get the annotation target and annotation instance corresponding to the
+                // (raw/abstract) indexed annotation
+                .map(indexedAnnotation -> getTarget(indexedAnnotation))
+                .forEach(annotationTarget -> 
+                    getAnnotationInstances(annotationTarget, webAnnotation)
+                        .forEach(annotationInstance ->  
+                            // Store the matching annotation instance (@WebServlet(name=...)
+                            // and annotation target (@WebServlet public class Target) in the manager
+                            annotationManager.addAnnotation(
+                                new DefaultAnnotationInfo<>(annotationInstance,  annotationTarget)))));
+            
+        
         DefaultWebApplicationServer webApplicationServer = new DefaultWebApplicationServer();
         webApplication.addFeature(new ServletFeature());
         webApplicationServer.addWebApplication(webApplication);
+        
+        
+        webApplication.addInitializer(new WebXmlInitializer());
+        webApplication.addInitializer(new WebAnnotationInitializer());
         
         webApplication.addInitializer(WeldInitializer.class.getName());
         
@@ -100,6 +155,41 @@ public class PiranhaServerDeployer {
 
     public void stop() {
         httpServer.stop();
+    }
+    
+    Index getIndex() {
+        ClassLoader classLoader= Thread.currentThread().getContextClassLoader();
+        
+        try (InputStream indexStream = classLoader.getResourceAsStream("META-INF/piranha.idx")) {
+            return new IndexReader(indexStream).read();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+    
+    void forEachWebAnnotation(Consumer<? super Class<?>> consumer) {
+        stream(webAnnotations).forEach(consumer);
+    }
+    
+    Stream<AnnotationInstance> getAnnotations(Index index, Class<?> webAnnotation) {
+        return 
+            index.getAnnotations(
+                    createSimple(webAnnotation.getName()))
+                 .stream();
+    }
+    
+    Class<?> getTarget (AnnotationInstance annotationInstance) {
+        try {
+            return Class.forName(annotationInstance.target().asClass().toString());
+        } catch (ClassNotFoundException e) {
+           throw new IllegalStateException(e);
+        }
+    }
+    
+    Stream<Annotation> getAnnotationInstances(Class<?> target, Class<?> annotationType) {
+        return stream(target.getAnnotations())
+                .filter(e -> e.annotationType().isAssignableFrom(annotationType));
+                
     }
     
 }
