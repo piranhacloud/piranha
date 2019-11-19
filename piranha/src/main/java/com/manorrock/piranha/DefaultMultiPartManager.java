23d7b1404a82b7622193efddb777458a87d92f5c
Manfred Riem <mriem@manorrock.com>
2019-11-19 16:12:01 -0700

    Fixes issue #373 - Refactor DefaultMultiPartManager

--- piranha/src/main/java/com/manorrock/piranha/DefaultMultiPartManager.java
+++ piranha/src/main/java/com/manorrock/piranha/DefaultMultiPartManager.java
@@ -29,47 +29,23 @@ package com.manorrock.piranha;
 
 import com.manorrock.piranha.api.MultiPartManager;
 import com.manorrock.piranha.api.WebApplicationRequest;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.Part;
 
 /**
- * The default multi-part manager.
+ * The default MultiPartManager.
  *
  * @author Manfred Riem (mriem@manorrock.com)
  */
 public class DefaultMultiPartManager implements MultiPartManager {
 
     /**
-     * Defines the BOUNDARY constant.
-     */
-    private static final String BOUNDARY = "boundary=";
-
-    /**
      * Stores the logger.
      */
     private static final Logger LOGGER = Logger.getLogger(DefaultMultiPartManager.class.getName());
-
-    /**
-     * Stores the boundary string.
-     */
-    private String boundary;
-
-    /**
-     * Stores the parsed flag.
-     */
-    private boolean parsed = false;
-
-    /**
-     * Stores the parts.
-     */
-    private final Map<String, Part> parts = new HashMap<>();
 
     /**
      * Get the parts.
@@ -79,10 +55,10 @@ public class DefaultMultiPartManager implements MultiPartManager {
      */
     @Override
     public Collection<Part> getParts(WebApplicationRequest request) {
-        if (!parsed) {
-            parse(request);
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "Getting parts for request: {0}", request);
         }
-        return Collections.unmodifiableCollection(parts.values());
+        return Collections.EMPTY_LIST;
     }
 
     /**
@@ -94,105 +70,9 @@ public class DefaultMultiPartManager implements MultiPartManager {
      */
     @Override
     public Part getPart(WebApplicationRequest request, String name) {
-        if (!parsed) {
-            parse(request);
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "Getting part: {0} for request: {0}", new Object[]{name, request});
         }
-        return parts.get(name);
-    }
-
-    /**
-     * Parse the request.
-     *
-     * @param request the web application request.
-     */
-    private void parse(WebApplicationRequest request) {
-        synchronized (request) {
-            if (!parsed) {
-                parsed = true;
-                parseBoundary(request);
-                parseParts(request);
-            }
-        }
-    }
-
-    /**
-     * Parse the boundary.
-     *
-     * @param request the web application request.
-     */
-    private void parseBoundary(WebApplicationRequest request) {
-        String contentType = request.getContentType();
-        int index = contentType.lastIndexOf(BOUNDARY);
-        boundary = contentType.substring(index + BOUNDARY.length());
-        if (boundary.charAt(0) == '"') {
-            index = boundary.lastIndexOf('"');
-            boundary = boundary.substring(1, index);
-        }
-        boundary = "--" + boundary;
-    }
-
-    /**
-     * Parse a line.
-     *
-     * @param inputStream the input stream.
-     */
-    private String parseLine(InputStream inputStream) {
         return null;
-    }
-
-    /**
-     * Parse a part.
-     *
-     * @param request the web application request.
-     * @param inputStream the input stream.
-     */
-    private Part parsePart(WebApplicationRequest request, InputStream inputStream) {
-        HashMap<String, String> headers = parsePartHeaders(request, inputStream);
-        return null;
-    }
-
-    /**
-     * Parse the parts.
-     *
-     * @param request the web application request.
-     */
-    private void parseParts(WebApplicationRequest request) {
-        try {
-            InputStream inputStream = request.getInputStream();
-            parsePreamble(request, inputStream);
-            Part part = parsePart(request, inputStream);
-            while (part != null) {
-                parts.put(part.getName(), part);
-                part = parsePart(request, inputStream);
-            }
-        } catch (IOException ioe) {
-            LOGGER.log(Level.WARNING, "An I/O error occurred while parsing parts", ioe);
-        }
-    }
-
-    /**
-     * Parse a preamble.
-     *
-     * @param request the web application request.
-     * @param inputStream the input stream.
-     */
-    private void parsePreamble(WebApplicationRequest request, InputStream inputStream) {
-        String line = parseLine(inputStream);
-        while (line != null) {
-            if (line.startsWith(boundary)) {
-                break;
-            }
-            line = parseLine(inputStream);
-        }
-    }
-
-    /**
-     * Parse the part headers.
-     *
-     * @param request the web application request.
-     * @param inputStream the input stream.
-     */
-    private HashMap<String, String> parsePartHeaders(WebApplicationRequest request, InputStream inputStream) {
-        return new HashMap<>();
     }
 }
