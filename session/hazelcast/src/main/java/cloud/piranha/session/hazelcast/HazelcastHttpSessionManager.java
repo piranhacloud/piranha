e7ccafaf0b069005f040be49bd36dd929d4ac783
Manfred Riem <mriem@manorrock.com>
2019-11-26 07:56:55 -0700

    Fixes issue #394 - Move session-hazelcast into session directory

--- session/hazelcast/src/main/java/cloud/piranha/session/hazelcast/HazelcastHttpSessionManager.java
+++ session/hazelcast/src/main/java/cloud/piranha/session/hazelcast/HazelcastHttpSessionManager.java
@@ -25,7 +25,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package com.manorrock.piranha.session.hazelcast;
+package cloud.piranha.session.hazelcast;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
