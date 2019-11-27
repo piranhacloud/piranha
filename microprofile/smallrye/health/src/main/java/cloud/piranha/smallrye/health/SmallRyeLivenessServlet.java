f1723e85fa64dd0af315322ccd00ed9c90e5ba18
Manfred Riem <mriem@manorrock.com>
2019-11-26 23:41:30 -0700

    Fixes issue #392 - Move Smallrye Health into microprofile directory

--- microprofile/smallrye/health/src/main/java/cloud/piranha/smallrye/health/SmallRyeLivenessServlet.java
+++ microprofile/smallrye/health/src/main/java/cloud/piranha/smallrye/health/SmallRyeLivenessServlet.java
@@ -25,7 +25,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package com.manorrock.piranha.smallrye.health;
+package cloud.piranha.smallrye.health;
 
 import io.smallrye.health.SmallRyeHealth;
 import io.smallrye.health.SmallRyeHealthReporter;
