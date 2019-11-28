54142b9be9396329605b08a7fcdb6f7b4eac608b
Manfred Riem <mriem@manorrock.com>
2019-11-28 09:40:13 -0700

    Fixes issue #450 - Refactor Arquillian support

--- arquillian/server/src/main/java/cloud/piranha/arquillian/server/PiranhaServerLoadableExtension.java
+++ arquillian/server/src/main/java/cloud/piranha/arquillian/server/PiranhaServerLoadableExtension.java
@@ -25,7 +25,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package com.manorrock.piranha.arquillian.server;
+package cloud.piranha.arquillian.server;
 
 import org.jboss.arquillian.container.spi.client.container.DeployableContainer;
 import org.jboss.arquillian.core.spi.LoadableExtension;
