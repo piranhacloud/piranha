8ac52efa67e098aac8471d5ce542c55f1d6af8a5
arjantijms <arjan.tijms@gmail.com>
2019-12-10 22:09:11 +0100

    #503 Implement Weld SPI to inject Principal
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

---
+++ cdi/weld/src/main/java/cloud/piranha/cdi/weld/WeldSecurityService.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2002-2019 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its 
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package cloud.piranha.cdi.weld;
+
+import java.security.Principal;
+
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.CDI;
+import javax.security.enterprise.SecurityContext;
+
+import org.jboss.weld.security.spi.SecurityServices;
+
+/**
+ * The implementation of this Weld SPI provides the current principal for injection by CDI.
+ * 
+ * <p>
+ * Implementation detail: In Weld 3.1.* org.jboss.weld.bean.builtin.ee.PrincipalBean calls this.
+ * 
+ * @author Arjan Tijms
+ *
+ */
+public class WeldSecurityService implements SecurityServices {
+
+    @Override
+    public Principal getPrincipal() {
+        Instance<SecurityContext> securityServiceInstance =  CDI.current().select(SecurityContext.class);
+        if (securityServiceInstance.isResolvable()) {
+            return securityServiceInstance.get().getCallerPrincipal();
+        }
+        
+        return null;
+    }
+
+    @Override
+    public void cleanup() {
+        // Nothing to do here
+    }
+    
+}
