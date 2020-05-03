9ee7ba2b69f89c417b70df7a4c44e18ff19e0a7d
Manfred Riem <mriem@manorrock.com>
2020-05-03 10:28:57 -0600

    Fixes issue #693 - Add SSL support

--- server/src/main/zip/bin/start-ssl.sh
+++ server/src/main/zip/bin/start-ssl.sh
@@ -10,6 +10,22 @@ else
     JAVA_BIN=${JAVA_HOME}/bin/java
 fi
 
-${JAVA_BIN} -Djavax.net.debug=ssl -Djava.util.logging.config.file=etc/logging.properties -jar lib/piranha-server.jar --ssl  &
+#
+# SSL_DEBUG - set the SSL debug mode which is useful for debugging SSL connections
+#             on the server side
+#
+# SSL_DEBUG=-Djavax.net.debug=ssl
+
+#
+# SSL_KEY_STORE - set the key store to use.
+#
+SSL_KEY_STORE=-Djavax.net.ssl.keyStore=etc/keystore.jks
+
+#
+# SSL_KEY_STORE_PASSWORD - set the key store password to use.
+#
+SSL_KEY_STORE_PASSWORD=-Djavax.net.ssl.keyStorePassword=password
+
+${JAVA_BIN} ${SSL_DEBUG} ${SSL_KEY_STORE} ${SSL_KEY_STORE_PASSWORD} -Djava.util.logging.config.file=etc/logging.properties -jar lib/piranha-server.jar --ssl  &
 
 echo $! >> tmp/piranha.pid
