502011b2b666b1c32375b830f5c2609e633c48bd
Manfred Riem <mriem@manorrock.com>
2020-05-09 09:53:55 -0600

    Fixes issue #719 - Align JavaDoc with website

--- micro/src/main/java/cloud/piranha/micro/package-info.java
+++ micro/src/main/java/cloud/piranha/micro/package-info.java
@@ -27,68 +27,17 @@
  */
 /**
  * <p>
- * The Piranha Micro package delivers you with a Servlet container that hosts
- * one web application.
+ * This package delivers you with a Servlet container that only hosts one web
+ * application.
  * </p>
- *
- * <h2>Installing Piranha Micro</h2>
- *
- * <p>
- * Download the JAR file from
- * <a href="https://repo1.maven.org/maven2/cloud/piranha/piranha-micro">Maven
- * Central</a>.
- * </p>
- *
- * <h2>Running with a WAR file</h2>
- *
- * <p>
- * If you have a WAR file you can use the command line below:
- * </p>
- *
- * <pre>
- * java -jar piranha-micro.jar --war your_webapplication.war
- * </pre>
- *
- * <h2>Overriding the web application directory</h2>
- *
- * <p>
- * If you want to override the directory Piranha Micro uses for extracting the
- * web application you can use the command line below:
- * </p>
- *
- * <pre>
- * java -jar piranha-micro.jar --war your_webapplication.war --webapp your_webapp_directory
- * </pre>
- *
- * <h2>Running with an exploded WAR file</h2>
- *
- * <p>
- * If you have an exploded directory containing the contents of your WAR file
- * you can use the command line below:
- * </p>
- *
- * <pre>
- * java -jar piranha-micro.jar --webapp your_webapp_directory
- * </pre>
- *
- * <h2>Changing the HTTP port</h2>
- *
- * <p>
- * If you want to use a different HTTP port (e.g to port 8888) you can use the
- * command line below:
- * </p>
- *
- * <pre>
- * java -jar piranha-micro.jar --war your_webapplication.war --port 8888
- * </pre>
  *
  * <h2>Architecture diagram</h2>
  *
  * <p>
  * The image below illustrates how the request and response handling is done by
  * Piranha Micro. When a request comes in to the HTTP server it dispatches it to
- * the {@link cloud.piranha.appserver.api.WebApplicationServer} which in
- * turn dispatches it to the WebApplication which then in turn uses
+ * the {@link cloud.piranha.appserver.api.WebApplicationServer} which in turn
+ * dispatches it to the WebApplication which then in turn uses
  * {@link cloud.piranha.webapp.api.WebApplicationRequestMapper} to determine
  * which FilterChain needs to process the incoming request and it dispatches to
  * it.
@@ -97,21 +46,12 @@
  * <p>
  * <img alt="Request and response handling" src="doc-files/request-response.png">
  * </p>
- *
- * <h2>Maven coordinates</h2>
- *
- * <pre>
- * &lt;dependency&gt;
- *   &lt;groupId&gt;cloud.piranha&lt;/groupId&gt;
- *   &lt;artifactId&gt;piranha-micro&lt;/artifactId&gt;
- *   &lt;version&gt;y.m.p&lt;/version&gt;
- *   &lt;version&gt;y.m.p&lt;/version&gt;
- * &lt;dependency&gt;
- * </pre>
+ * 
+ * <h2>How do I use Piranha Micro?</h2>
  *
  * <p>
- * where y is the year, m is the month and p is the patch version of the release
- * you want to use.
+ * See our <a href="https://piranha.cloud/micro/">documentation</a> for more
+ * information.
  * </p>
  *
  * @author Manfred Riem (mriem@manorrock.com)
