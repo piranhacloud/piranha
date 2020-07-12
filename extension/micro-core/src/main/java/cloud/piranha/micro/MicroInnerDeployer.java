a31d8ffdaa7d87043a6594f2b26cc1586ce978ae
arjantijms <arjan.tijms@gmail.com>
2020-07-12 18:47:33 +0200

    #957 load piranha-callers.xml from file in archive
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- extension/micro-core/src/main/java/cloud/piranha/micro/MicroInnerDeployer.java
+++ extension/micro-core/src/main/java/cloud/piranha/micro/MicroInnerDeployer.java
@@ -1,32 +1,33 @@
 /*
  * Copyright (c) 2002-2020 Manorrock.com. All Rights Reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *   1. Redistributions of source code must retain the above copyright notice, 
+ *   1. Redistributions of source code must retain the above copyright notice,
  *      this list of conditions and the following disclaimer.
  *   2. Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of the copyright holder nor the names of its 
+ *   3. Neither the name of the copyright holder nor the names of its
  *      contributors may be used to endorse or promote products derived from
  *      this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
 package cloud.piranha.micro;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Arrays.asList;
 import static java.util.Arrays.stream;
 import static javax.naming.Context.INITIAL_CONTEXT_FACTORY;
@@ -88,25 +89,25 @@ import cloud.piranha.webapp.impl.DefaultWebApplicationExtensionContext;
 
 /**
  * Deploys a shrinkwrap application archive to a newly started embedded Piranha instance.
- * 
+ *
  * <p>
  * This class is expected to be run within in its own inner (isolated) class loader
- * 
+ *
  * @author arjan
  *
  */
 public class MicroInnerDeployer {
-    
+
     /**
      * Defines the attribute name for the MicroPiranha reference.
      */
     static final String MICRO_PIRANHA = "cloud.piranha.micro.MicroPiranha";
-    
+
     private static final Logger LOGGER = Logger.getLogger(MicroInnerDeployer.class.getName());
-    
+
     Class<?>[] webAnnotations = new Class<?>[] {
        // Servlet
-       WebServlet.class, 
+       WebServlet.class,
        WebListener.class,
        WebInitParam.class,
        WebFilter.class,
@@ -118,20 +119,20 @@ public class MicroInnerDeployer {
        Provider.class,
        ApplicationPath.class,
     };
-    
+
     Class<?>[] instances = new Class<?>[] {
         // REST
         Application.class,
     };
-    
+
     private HttpServer httpServer;
-    
+
     public Set<String> start(Archive<?> applicationArchive, ClassLoader classLoader, Map<String, Function<URL, URLConnection>> handlers, Integer port) {
         try {
             System.getProperties().put(INITIAL_CONTEXT_FACTORY, DynamicInitialContextFactory.class.getName());
-            
+
             WebApplication webApplication = getWebApplication(applicationArchive, classLoader);
-            
+
             LOGGER.info(
                 "Starting web application " + applicationArchive.getName() + " on Piranha Micro " + webApplication.getAttribute(MICRO_PIRANHA));
 
@@ -140,16 +141,16 @@ public class MicroInnerDeployer {
             // Such URLs come into being primarily when code takes resolves a class or resource from the class loader by URL
             // and then takes the string form of the URL representing the class or resource.
             GlobalArchiveStreamHandler streamHandler = new GlobalArchiveStreamHandler(webApplication);
-            
+
             // Life map to the StaticURLStreamHandlerFactory used by the root class loader
             handlers.put("shrinkwrap", e -> streamHandler.connect(e));
-            
+
             // Source of annotations
             Index index = getIndex();
-            
+
             // Target of annotations
             DefaultAnnotationManager annotationManager = (DefaultAnnotationManager) webApplication.getAnnotationManager();
-            
+
             // Copy from source index to target manager
             forEachWebAnnotation(webAnnotation ->
                 // Read the web annotations (@WebServlet.class etc) from the source index
@@ -157,33 +158,33 @@ public class MicroInnerDeployer {
                     // Get the annotation target and annotation instance corresponding to the
                     // (raw/abstract) indexed annotation
                     .map(indexedAnnotation -> getTarget(indexedAnnotation))
-                    .forEach(annotationTarget -> 
+                    .forEach(annotationTarget ->
                         getAnnotationInstances(annotationTarget, webAnnotation)
-                            .forEach(annotationInstance ->  
+                            .forEach(annotationInstance ->
                                 // Store the matching annotation instance (@WebServlet(name=...)
                                 // and annotation target (@WebServlet public class Target) in the manager
                                 annotationManager.addAnnotation(
                                     new DefaultAnnotationInfo<>(annotationInstance,  annotationTarget)))));
-            
+
             // Collect sub-classes of our "instances" collection
             forEachInstance(instanceClass ->
                 getInstances(index, instanceClass)
                     .map(indexedInstance -> getTarget(indexedInstance))
-                    .forEach(implementingClass -> 
+                    .forEach(implementingClass ->
                         annotationManager.addInstance(instanceClass, implementingClass)));
-                        
-            
-            getCallerCredentials(System.getProperty("io.piranha.identitystore.callers"));
-            
+
+
+            initIdentityStore(webApplication);
+
             DefaultWebApplicationServer webApplicationServer = new DefaultWebApplicationServer();
             webApplicationServer.addWebApplication(webApplication);
-            
+
             DefaultWebApplicationExtensionContext extensionContext = new DefaultWebApplicationExtensionContext();
             for (WebApplicationExtension extension : ServiceLoader.load(WebApplicationExtension.class)) {
                 extensionContext.add(extension);
             }
             extensionContext.configure(webApplication);
-            
+
             webApplicationServer.initialize();
             webApplicationServer.start();
 
@@ -193,8 +194,11 @@ public class MicroInnerDeployer {
             httpServer.setSSL(Boolean.getBoolean("piranha.http.ssl"));
             httpServer.setHttpServerProcessor(webApplicationServer);
             httpServer.start();
-            
+
             return webApplication.getServletRegistrations().keySet();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalStateException(e);
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
@@ -205,7 +209,7 @@ public class MicroInnerDeployer {
         WebApplication webApplication = new DefaultWebApplication();
         webApplication.setClassLoader(newClassLoader);
         webApplication.addResource(new ShrinkWrapResource(archive));
-        
+
         // Set version
         webApplication.setAttribute(MICRO_PIRANHA, new Piranha() {
             @Override
@@ -217,7 +221,7 @@ public class MicroInnerDeployer {
                 return getVersion();
             }
         });
-        
+
         return webApplication;
     }
 
@@ -226,28 +230,28 @@ public class MicroInnerDeployer {
             httpServer.stop();
         }
     }
-    
+
     Index getIndex() {
         ClassLoader classLoader= Thread.currentThread().getContextClassLoader();
-        
+
         try (InputStream indexStream = classLoader.getResourceAsStream("META-INF/piranha.idx")) {
             return new IndexReader(indexStream).read();
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
     }
-    
+
     void forEachWebAnnotation(Consumer<? super Class<?>> consumer) {
         stream(webAnnotations).forEach(consumer);
     }
-    
+
     Stream<AnnotationInstance> getAnnotations(Index index, Class<?> webAnnotation) {
-        return 
+        return
             index.getAnnotations(
                     createSimple(webAnnotation.getName()))
                  .stream();
     }
-    
+
     Stream<ClassInfo> getInstances(Index index, Class<?> instanceClass) {
         return
             Stream.concat(
@@ -258,19 +262,19 @@ public class MicroInnerDeployer {
                          createSimple(instanceClass.getName()))
                       .stream());
     }
-    
+
     Class<?> getTarget (AnnotationInstance annotationInstance) {
         return getTarget(annotationInstance.target());
     }
-    
+
     Class<?> getTarget (AnnotationTarget target) {
         try {
             if (target.kind() == CLASS) {
                 return Class.forName(
-                    target.asClass().toString(), true, 
+                    target.asClass().toString(), true,
                     Thread.currentThread().getContextClassLoader());
             }
-            
+
             return Class.forName(
                 target.asMethod().declaringClass().toString(), true,
                 Thread.currentThread().getContextClassLoader());
@@ -278,52 +282,63 @@ public class MicroInnerDeployer {
             throw new IllegalStateException(e);
         }
     }
-    
+
     Stream<Annotation> getAnnotationInstances(Class<?> target, Class<?> annotationType) {
         return stream(target.getAnnotations())
                 .filter(e -> e.annotationType().isAssignableFrom(annotationType));
     }
-    
+
     void forEachInstance(Consumer<? super Class<?>> consumer) {
         stream(instances).forEach(consumer);
     }
-    
+
     void getCallerCredentials(String callersAsXml) {
         if (callersAsXml == null || callersAsXml.isEmpty()) {
             return;
         }
-        
+
         try {
-            
+
             XPath xPath = XPathFactory
             .newInstance()
             .newXPath();
-            
-            NodeList nodes = (NodeList) 
+
+            NodeList nodes = (NodeList)
                 xPath
                     .evaluate(
-                        "//caller", 
+                        "//caller",
                         DocumentBuilderFactory
                             .newInstance()
                             .newDocumentBuilder()
-                            .parse(new ByteArrayInputStream(callersAsXml.getBytes())), 
+                            .parse(new ByteArrayInputStream(callersAsXml.getBytes())),
                         NODESET);
-            
+
             for (int i = 0; i < nodes.getLength(); i++) {
                 NamedNodeMap callerAttributes = nodes.item(i).getAttributes();
-                
+
                 String caller = callerAttributes.getNamedItem("callername").getNodeValue();
-                String password = callerAttributes.getNamedItem("password").getNodeValue(); 
+                String password = callerAttributes.getNamedItem("password").getNodeValue();
                 String groups = callerAttributes.getNamedItem("groups").getNodeValue();
-                
+
                 InMemoryIdentityStore.addCredential(caller, password, asList(groups.split(",")));
             }
-        
+
         } catch (SAXException | IOException | ParserConfigurationException | XPathExpressionException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
+
     }
-    
+
+    void initIdentityStore(WebApplication webApplication) throws IOException {
+        String callers = System.getProperty("io.piranha.identitystore.callers");
+        if (callers == null) {
+            InputStream xmlStream = webApplication.getResourceAsStream("WEB-INF/piranha-callers.xml");
+            if (xmlStream != null) {
+                callers = new String(xmlStream.readAllBytes(), UTF_8);
+            }
+        }
+
+        getCallerCredentials(callers);
+    }
+
 }
