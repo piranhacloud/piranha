dac074447eac3de9b02ce4223586c5bb613c285c
Manfred Riem <mriem@manorrock.com>
2020-07-24 22:21:25 -0600

    Fixes issue #981 - Add Jakarta EE extension skeleton

--- micro/src/main/java/cloud/piranha/micro/MicroConfiguration.java
+++ micro/src/main/java/cloud/piranha/micro/MicroConfiguration.java
@@ -62,7 +62,7 @@ public class MicroConfiguration {
     public MicroConfiguration() {
         this(
             System.getProperty("piranha.version", MicroConfiguration.class.getPackage().getImplementationVersion()),
-            System.getProperty("piranha.extensions", "micro-core,micro-servlet"), 
+            System.getProperty("piranha.extensions", "micro-core,micro"), 
                 System.getProperty("piranha.dependencies", ""),
             System.getProperty("piranha.repositories", "https://repo1.maven.org/maven2"), 
             Boolean.valueOf(System.getProperty("piranha.offline", "false")),
