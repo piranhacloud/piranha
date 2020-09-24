b63b06aa939c838fac87b63a598efe5eaaff33b5
Arjan Tijms <arjan.tijms@gmail.com>
2020-09-24 18:28:59 +0200

    #1191 - Introduce server variant building on Micro (#1192)
    #1191 - Introduce server variant building on Micro
    
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

---
+++ server2/src/main/zip/bin/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ..
+
+if [ -z ${JAVA_HOME} ]; then
+    echo Using default java
+    JAVA_BIN=java 
+else
+    echo Using JAVA_HOME: ${JAVA_HOME}
+    JAVA_BIN=${JAVA_HOME}/bin/java
+fi
+
+exec ${JAVA_BIN} ${JAVA_ARGS} -Djava.util.logging.config.file=etc/logging.properties -jar lib/piranha-server.jar &
+
+PID=$!
+echo $PID >> tmp/piranha.pid
+wait $PID
