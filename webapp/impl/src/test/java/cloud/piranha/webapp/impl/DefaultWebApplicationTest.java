904bc4070b412891785d63e337e802d176c91e4f
Thiago Henrique Hüpner <thihup@gmail.com>
2020-10-01 11:27:34 -0300

    Fixes issue #1217 - Make sure to set the character encoding at getWriter (#1218)

--- webapp/impl/src/test/java/cloud/piranha/webapp/impl/DefaultWebApplicationTest.java
+++ webapp/impl/src/test/java/cloud/piranha/webapp/impl/DefaultWebApplicationTest.java
@@ -35,6 +35,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.Enumeration;
@@ -1098,5 +1099,14 @@ class DefaultWebApplicationTest {
         response.setWebApplication(webApp);
         response.setLocale(Locale.ITALY);
         assertEquals("windows-1252", response.getCharacterEncoding());
+    }
+
+    @Test
+    void testGetContentType() throws Exception {
+        DefaultWebApplicationResponse response = new DefaultWebApplicationResponse();
+        assertNull(response.getContentType());
+        PrintWriter writer = response.getWriter();
+        response.setContentType("text/html");
+        assertEquals("text/html;charset=ISO-8859-1", response.getContentType());
     }
 }
