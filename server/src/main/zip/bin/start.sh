d4d60abee70337e9cddfc5f41fdee3be0371f0dc
Thiago Henrique Hüpner <thihup@gmail.com>
2020-10-13 20:05:43 -0300

    Fixes issue #1228 - Start Piranha Server using JPMS by default (#1230)

--- server/src/main/zip/bin/start.sh
+++ server/src/main/zip/bin/start.sh
@@ -10,6 +10,12 @@ else
     JAVA_BIN=${JAVA_HOME}/bin/java
 fi
 
+if [[ "${PIRANHA_JPMS}" == "false" ]]; then
+    INIT_OPTIONS="-jar lib/piranha-server.jar"
+else
+    INIT_OPTIONS="--module-path lib -m cloud.piranha.server"
+fi
+
 #
 # Turn SSL on.
 #
@@ -31,7 +37,7 @@ fi
 # SSL_KEY_STORE_PASSWORD=-Djavax.net.ssl.keyStorePassword=password
 
 ${JAVA_BIN} ${JAVA_ARGS} ${SSL_DEBUG} ${SSL_KEY_STORE} ${SSL_KEY_STORE_PASSWORD} \
-  -Djava.util.logging.config.file=etc/logging.properties -jar \
-  lib/piranha-server.jar ${SSL_ON} &
+  -Djava.util.logging.config.file=etc/logging.properties \
+  ${INIT_OPTIONS} ${SSL_ON} &
 
 echo $! >> tmp/piranha.pid
