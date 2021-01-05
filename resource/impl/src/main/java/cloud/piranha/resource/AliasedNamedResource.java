c9241a8638f479af38f7af1d01d9014d4cec3b2e
Thiago Henrique Hupner <thihup@gmail.com>
2021-01-05 13:32:10 -0300

    Add AliasedNamedResource
    This wrapper allows to return a specific value for the Resource::getName

---
+++ resource/impl/src/main/java/cloud/piranha/resource/AliasedNamedResource.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2002-2021 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package cloud.piranha.resource;
+
+import cloud.piranha.resource.api.Resource;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.stream.Stream;
+
+/**
+ * A {@link Resource} wrapper with a custom name
+ */
+public class AliasedNamedResource implements Resource {
+
+    /**
+     * Stores the resource
+     */
+    private final Resource resource;
+
+    /**
+     * Stores the resource name
+     */
+    private final String resourceName;
+
+    /**
+     * Constructor
+     * @param resource the resource
+     * @param resourceName the resource name
+     */
+    public AliasedNamedResource(Resource resource, String resourceName) {
+        this.resource = resource;
+        this.resourceName = resourceName;
+    }
+
+    @Override
+    public URL getResource(String location) {
+        return resource.getResource(location);
+    }
+
+    @Override
+    public InputStream getResourceAsStream(String location) {
+        return resource.getResourceAsStream(location);
+    }
+
+    @Override
+    public Stream<String> getAllLocations() {
+        return resource.getAllLocations();
+    }
+
+    @Override
+    public String getName() {
+        return resourceName;
+    }
+
+}
