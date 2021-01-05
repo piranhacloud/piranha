f477c08a4f441badc32d84c304ba7ab426937db0
Thiago Henrique Hupner <thihup@gmail.com>
2021-01-05 11:13:19 -0300

    Add tests to ModuleFinder

--- jpms/src/test/java/package1/Servlet.java
+++ jpms/src/test/java/package1/Servlet.java
@@ -4,14 +4,14 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of the copyright holder nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -24,24 +24,10 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 
-/**
- * The Piranha - JPMS integration module.
- *
- * <p>
- *  This module delivers the default implementation of the {@link java.lang.module} API.
- * </p>
- *
- * @author Thiago Henrique Hupner
- */
-module cloud.piranha.jpms {
+package package1;
 
-    exports cloud.piranha.jpms;
-
-    requires cloud.piranha.resource.api;
-    requires java.logging;
-}
-
-module cloud.piranha.jpms.tests {
+public class Servlet {
 }
