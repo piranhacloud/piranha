bd72b6f478f8832bae6060099121949ed17c0ccb
Arjan Tijms <arjan.tijms@gmail.com>
2021-01-22 19:25:07 +0100

    #1465 Add code to make Micro a bit easier to embed by end users. (#1466)
    * #1465 Add code to make Micro a bit easier to embed by end users.
    
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

---
+++ micro-embedded/src/main/java/cloud/piranha/micro/embedded/MicroEmbeddedPiranhaBuilder.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2002-2021 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package cloud.piranha.micro.embedded;
+
+import org.jboss.shrinkwrap.api.Archive;
+
+import cloud.piranha.micro.MicroConfiguration;
+import cloud.piranha.micro.MicroOuterDeployer;
+import cloud.piranha.naming.thread.ThreadInitialContextFactory;
+import cloud.piranha.policy.thread.ThreadPolicy;
+
+/**
+ * Builder for an embedded Piranha instance based on Piranha Micro
+ * 
+ * @author Arjan Tijms
+ */
+public class MicroEmbeddedPiranhaBuilder {
+
+	/**
+	 * Object containing all configuration settings for Piranha Micro
+	 */
+	private MicroConfiguration configuration;
+	
+	/**
+	 * Application archive that will be executed by Piranha Micro
+	 */
+	private Archive<?> archive;
+
+	/**
+	 * Sets the configuration for Piranha Micro
+	 * 
+	 * @param configuration the configuration
+	 * @return instance of this builder
+	 */
+	public MicroEmbeddedPiranhaBuilder configuration(MicroConfiguration configuration) {
+		this.configuration = configuration;
+		return this;
+	}
+
+	/**
+	 * Set the application archive that will be loaded and executed by Piranha Micro
+	 * 
+	 * @param archive the archive to be executed
+	 * @return instance of this builder
+	 */
+	public MicroEmbeddedPiranhaBuilder archive(Archive<?> archive) {
+		this.archive = archive;
+		return this;
+	}
+
+	/**
+	 * Builds an embedded Piranha Micro instance and deploys the archive set by this builder to it.
+	 * @return the newly created Piranha Micro instance
+	 */
+	public MicroEmbeddedPiranha buildAndStart() {
+		MicroEmbeddedPiranha microEmbeddedPiranha = new MicroEmbeddedPiranha();
+		MicroWebApplication microWebApplication = microEmbeddedPiranha.getWebApplication();
+		if (configuration == null) {
+			// Use default config is nothing set
+			configuration = new MicroConfiguration();
+			configuration.setHttpStart(false);
+		}
+
+		if (configuration.getRoot() != null) {
+			// If an explicit root is set, use it. Otherwise use the default.
+			microWebApplication.setContextPath(configuration.getRoot());
+		}
+
+		try {
+			ThreadPolicy.setPolicy(microWebApplication.getPolicyManager().getPolicy());
+			ThreadInitialContextFactory.setInitialContext(microWebApplication.getNamingManager().getContext());
+
+			microWebApplication.setDeployedApplication(
+					new MicroOuterDeployer(configuration.postConstruct()).deploy(archive).getDeployedApplication());
+			
+			if (!configuration.isHttpStart()) {
+			    ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
+	            try {
+	                Thread.currentThread().setContextClassLoader(microWebApplication.getClassLoader());
+	                microWebApplication.initialize();
+	                microWebApplication.start();
+	            } finally {
+	                Thread.currentThread().setContextClassLoader(oldClassLoader);
+	            }
+			}
+			
+			return microEmbeddedPiranha;
+		} finally {
+			ThreadPolicy.removePolicy();
+			ThreadInitialContextFactory.removeInitialContext();
+		}
+	}
+
+}
---
+++ test/mojarra2/src/test/java/cloud/piranha/test/faces/mojarra/Mojarra2Test.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2002-2021 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its 
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package cloud.piranha.test.faces.mojarra;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.jupiter.api.Test;
+
+import cloud.piranha.micro.embedded.MicroEmbeddedPiranhaBuilder;
+
+/**
+ * The JUnit tests for the Hello Jakarta Faces web application.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ * @author Arjan Tijms (arjan.tijms@gmail.com)
+ */
+class Mojarra2Test {
+
+    /**
+     * Test /index.html.
+     *
+     * @throws Exception
+     */
+    @Test
+    void testIndexHtml1() throws Exception {
+        
+        System.out.println(Runtime.version().version());
+        
+        // See https://github.com/piranhacloud/piranha/issues/1467
+        if (Runtime.version().feature() > 15) {
+            return;
+        }
+        
+    	String result =
+    	new MicroEmbeddedPiranhaBuilder()
+    		.archive(
+				ShrinkWrap
+					.create(WebArchive.class)
+	                  .addAsWebResource(new StringAsset( """
+                          <!DOCTYPE html>
+
+                          <html lang="en" xmlns:h="http://xmlns.jcp.org/jsf/html">
+                              <h:head>
+                                  <title>Hello Jakarta Faces</title>
+                              </h:head>    
+                              <h:body>
+                                  Hello Jakarta Faces
+                              </h:body>
+                          </html>
+                          """), "index.xhtml")
+	                  .addAsWebInfResource(EmptyAsset.INSTANCE, "faces-config.xml")
+	                  .addAsLibraries(
+                		  Maven.resolver()
+                		       .resolve(
+            		    		   //"org.glassfish:jakarta.faces:3.0.0", // Needs fix for #1444
+            		    		   "jakarta.websocket:jakarta.websocket-api:2.0.0"
+                		    	)
+                		       .withTransitivity().as(JavaArchive.class)))
+        		.buildAndStart()
+        		.service("/index.xhtml")
+        		.getResponseAsString();
+    		
+        System.out.println(result);
+        assertTrue(result.contains("Hello Jakarta Faces"));
+    }
+}
