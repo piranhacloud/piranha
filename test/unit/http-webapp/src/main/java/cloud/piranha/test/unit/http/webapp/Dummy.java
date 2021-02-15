af4c605d7a63fdf977a6c50fe354b6bd7a04ddd5
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-02-15 08:53:54 -0700

    Fixes issue #1494 - Move piranha-http-webapp-test module into test directory

--- test/unit/http-webapp/src/main/java/cloud/piranha/test/unit/http/webapp/Dummy.java
+++ test/unit/http-webapp/src/main/java/cloud/piranha/test/unit/http/webapp/Dummy.java
@@ -25,7 +25,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package cloud.piranha.http.webapp.tests;
+package cloud.piranha.test.unit.http.webapp;
 
 public class Dummy {
 
