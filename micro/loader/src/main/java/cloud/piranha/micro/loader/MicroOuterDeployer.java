7a2a356599324be12c6e2142d704a883c572dc99
arjantijms <arjan.tijms@gmail.com>
2021-03-18 19:44:09 +0100

    Checkstyle
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- micro/loader/src/main/java/cloud/piranha/micro/loader/MicroOuterDeployer.java
+++ micro/loader/src/main/java/cloud/piranha/micro/loader/MicroOuterDeployer.java
@@ -44,7 +44,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.jboss.jandex.Index;
