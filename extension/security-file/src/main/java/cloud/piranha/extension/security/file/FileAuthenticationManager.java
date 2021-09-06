e224408f05e38bb272bd0badf2b64bd026903e26
Manfred Riem <mriem@manorrock.com>
2021-09-06 08:55:13 -0600

    Fixes issue #1890 - Add FORM based authentication to FileAuthenticationManager

--- extension/security-file/src/main/java/cloud/piranha/extension/security/file/FileAuthenticationManager.java
+++ extension/security-file/src/main/java/cloud/piranha/extension/security/file/FileAuthenticationManager.java
@@ -28,48 +28,112 @@
 package cloud.piranha.extension.security.file;
 
 import cloud.piranha.webapp.api.AuthenticationManager;
+import cloud.piranha.webapp.api.WebApplicationRequest;
+import cloud.piranha.webapp.impl.DefaultSecurityPrincipal;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import static jakarta.servlet.http.HttpServletRequest.FORM_AUTH;
+import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
 
 /**
  * The file-based authentication manager.
- * 
+ *
  * @author Manfred Riem (mriem@manorrock.com)
  */
 public class FileAuthenticationManager implements AuthenticationManager {
-    
+
     /**
      * Stores the users file.
      */
     private File usersFile;
-    
+
+    /**
+     * Stores the logins.
+     */
+    private final HashMap<String, String> logins = new HashMap<>();
+
     /**
      * Constructor.
-     * 
+     *
      * @param usersFile the users file.
      */
     public FileAuthenticationManager(File usersFile) {
         this.usersFile = usersFile;
+
+        Properties userProperties = new Properties();
+        try ( FileInputStream fileInput = new FileInputStream(usersFile)) {
+            userProperties.load(fileInput);
+        } catch (IOException ioe) {
+        }
+        userProperties.entrySet().forEach(entry -> {
+            String username = (String) entry.getKey();
+            String password = (String) entry.getValue();
+            logins.put(username, password);
+        });
     }
 
     @Override
-    public boolean authenticate(HttpServletRequest request, 
+    public boolean authenticate(HttpServletRequest request,
             HttpServletResponse response) throws IOException, ServletException {
+        if (request.getAuthType() != null) {
+            switch (request.getAuthType()) {
+                case FORM_AUTH -> {
+                    return authenticateForm(request, response);
+                }
+            }
+        }
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * Authenticate FORM based.
+     *
+     * @param request the request,
+     * @param response the response.
+     */
+    private boolean authenticateForm(HttpServletRequest request,
+            HttpServletResponse response) throws IOException, ServletException {
+
+        boolean result = false;
+        String username = request.getParameter("j_username");
+        String password = request.getParameter("j_password");
+        login(request, username, password);
+        if (request.getUserPrincipal() != null) {
+            result = true;
+        }
+        return result;
+    }
+
     @Override
-    public void login(HttpServletRequest request, String username, 
+    public void login(HttpServletRequest request, String username,
             String password) throws ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        if (logins.containsKey(username) && password != null && password.equals(logins.get(username))) {
+            while (request instanceof HttpServletRequestWrapper wrapper) {
+                request = (HttpServletRequest) wrapper.getRequest();
+            }
+            if (request instanceof WebApplicationRequest webAppRequest) {
+                webAppRequest.setUserPrincipal(new DefaultSecurityPrincipal(username));
+            }
+        } else {
+            throw new ServletException("Unable to login using the given username and/or password");
+        }
     }
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response)
             throws ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        while (request instanceof HttpServletRequestWrapper wrapper) {
+            request = (HttpServletRequest) wrapper.getRequest();
+        }
+        if (request instanceof WebApplicationRequest webAppRequest) {
+            webAppRequest.setUserPrincipal(null);
+        }
     }
 }
