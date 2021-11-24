f3e81216098244e55d98f2b7e71a037a39b78807
Manfred Riem <mriem@manorrock.com>
2021-11-23 19:54:35 -0700

    Fixes issue #2109 - Create a Maven plugin for Piranha Micro to enable inner loop development

--- micro/core/src/main/java/cloud/piranha/micro/core/MicroPiranha.java
+++ micro/core/src/main/java/cloud/piranha/micro/core/MicroPiranha.java
@@ -261,8 +261,9 @@ public class MicroPiranha implements Piranha, Runnable {
         LOGGER.log(INFO, "Started Piranha");
         LOGGER.log(INFO, "It took {0} milliseconds", finishTime - startTime);
 
+        File pidFile = new File("tmp", "piranha-micro.pid");
+                
         if (pid != null) {
-            File pidFile = new File("tmp", "piranha-micro.pid");
             if (!pidFile.getParentFile().exists()) {
                 if (pidFile.getParentFile().mkdirs()) {
                     LOGGER.log(WARNING, "Unable to create tmp directory for PID file");
@@ -278,8 +279,6 @@ public class MicroPiranha implements Piranha, Runnable {
 
         while (!stop) {
             if (pid != null) {
-                System.out.println("PID FILE CHECK");
-                File pidFile = new File("tmp", "piranha-micro.pid");
                 if (!pidFile.exists()) {
                     stop();
                 }
