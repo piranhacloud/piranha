e601f211b3be861bd425b68eb9388f0d7c49f7a6
Manfred Riem <mriem@manorrock.com>
2022-01-01 08:47:58 -0700

    Fixes issue #2304 - Update copyright year

--- resource/impl/src/main/java/module-info.java
+++ resource/impl/src/main/java/module-info.java
@@ -1,9 +1,5 @@
-import java.net.spi.URLStreamHandlerProvider;
-
-import cloud.piranha.resource.impl.ByteArrayResourceStreamHandlerProvider;
-
 /*
- * Copyright (c) 2002-2021 Manorrock.com. All Rights Reserved.
+ * Copyright (c) 2002-2022 Manorrock.com. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -29,6 +25,9 @@ import cloud.piranha.resource.impl.ByteArrayResourceStreamHandlerProvider;
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+import java.net.spi.URLStreamHandlerProvider;
+import cloud.piranha.resource.impl.ByteArrayResourceStreamHandlerProvider;
 
 /**
  * The Piranha - Resource - Implementation module.
--- resource/impl/src/test/java/cloud/piranha/resource/impl/tests/AliasedNamedResourceTest.java
+++ resource/impl/src/test/java/cloud/piranha/resource/impl/tests/AliasedNamedResourceTest.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (c) 2002-2021 Manorrock.com. All Rights Reserved.
+ * Copyright (c) 2002-2022 Manorrock.com. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *  1. Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *  2. Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *  3. Neither the name of the copyright holder nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -24,9 +24,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
  */
-
 package cloud.piranha.resource.impl.tests;
 
 import cloud.piranha.resource.impl.AliasedNamedResource;
