9bd499602e2c44beb6bf25aea9c26572299001e2
Manfred Riem <mriem@manorrock.com>
2022-01-08 08:26:44 -0700

    Fixes issue #2319 - Fix heirarchyErrorMatchTest test

--- core/impl/src/main/java/cloud/piranha/core/impl/DefaultServletRequestDispatcher.java
+++ core/impl/src/main/java/cloud/piranha/core/impl/DefaultServletRequestDispatcher.java
@@ -367,8 +367,8 @@ public class DefaultServletRequestDispatcher implements RequestDispatcher {
 
             if (throwable instanceof ServletException servletException) {
                 errorRequest.setAttribute(ERROR_EXCEPTION, servletException.getRootCause());
-                errorRequest.setAttribute(ERROR_EXCEPTION_TYPE, servletException.getRootCause() == null ? null : servletException.getCause().getClass());
-                errorRequest.setAttribute(ERROR_MESSAGE, servletException.getRootCause() == null ? null : servletException.getCause().getMessage());
+                errorRequest.setAttribute(ERROR_EXCEPTION_TYPE, servletException.getRootCause() == null ? null : servletException.getRootCause().getClass());
+                errorRequest.setAttribute(ERROR_MESSAGE, servletException.getRootCause() == null ? null : servletException.getRootCause().getMessage());
             } else {
                 errorRequest.setAttribute(ERROR_EXCEPTION, throwable);
                 errorRequest.setAttribute(ERROR_EXCEPTION_TYPE, throwable == null ? null : throwable.getClass());
