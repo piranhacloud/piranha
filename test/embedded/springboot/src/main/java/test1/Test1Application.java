7b76ae114247584cde624d2d841aa77bdc264f71
Manfred Riem <mriem@manorrock.com>
2022-12-13 08:41:20 -0600

    Fixes issue #3153 - Apply fixes from 22.12.0 release branch (#3154)

--- test/embedded/springboot/src/main/java/test1/Test1Application.java
+++ test/embedded/springboot/src/main/java/test1/Test1Application.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2002-2022 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package test1;
 
 import cloud.piranha.spring.starter.embedded.EmbeddedPiranhaServletWebServerFactory;
@@ -7,6 +34,11 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * The Spring boot test application.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
 @SpringBootApplication
 public class Test1Application {
 
@@ -28,6 +60,11 @@ public class Test1Application {
     @Bean
     public WebServerFactoryCustomizer<EmbeddedPiranhaServletWebServerFactory> customizer() {
         return new WebServerFactoryCustomizer<EmbeddedPiranhaServletWebServerFactory>() {
+            /**
+             * Customize the factory.
+             * 
+             * @param factory the factory to customize.
+             */
             @Override
             public void customize(EmbeddedPiranhaServletWebServerFactory factory) {
                 factory.setPort(8080);
@@ -35,7 +72,12 @@ public class Test1Application {
         };
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Test1Application.class, args);
+    /**
+     * Main method.
+     * 
+     * @param arguments the command-line arguments. 
+     */
+    public static void main(String[] arguments) {
+        SpringApplication.run(Test1Application.class, arguments);
     }
 }
