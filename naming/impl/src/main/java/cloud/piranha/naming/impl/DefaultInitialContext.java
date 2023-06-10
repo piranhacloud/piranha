9ccb4556470576badae04186e4606cd21e581443
Manfred Riem <mriem@manorrock.com>
2023-06-10 09:55:43 -0500

    Undeprecate Piranha Naming (#3425)

--- naming/impl/src/main/java/cloud/piranha/naming/impl/DefaultInitialContext.java
+++ naming/impl/src/main/java/cloud/piranha/naming/impl/DefaultInitialContext.java
@@ -49,9 +49,7 @@ import javax.naming.OperationNotSupportedException;
  * The default InitialContext.
  *
  * @author Manfred Riem (mriem@manorrock.com)
- * @deprecated
  */
-@Deprecated(since = "23.5.0", forRemoval = true)
 public class DefaultInitialContext implements Context {
 
     /**
