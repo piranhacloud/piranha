6676719d803a1d3bbc9efacee914248c1eea9e97
Manfred Riem <mriem@manorrock.com>
2023-10-14 11:09:09 -0500

    Fixes #3540 - Update to Java 21 (#3561)

--- core/impl/src/test/java/cloud/piranha/core/impl/DefaultPushBuilderTest.java
+++ core/impl/src/test/java/cloud/piranha/core/impl/DefaultPushBuilderTest.java
@@ -30,7 +30,7 @@ package cloud.piranha.core.impl;
 import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
