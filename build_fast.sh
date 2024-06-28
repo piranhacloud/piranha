c0c0e3b13b8d12ae5f55cbbef920758983a8eb9c
Manfred Riem <mriem@manorrock.com>
2024-06-28 16:29:23 -0500

    Fixes #3790 - Add Maven config (#3791)

--- build_fast.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Fast builder for Piranha. Excludes tests and test modules specifically incompatible with fast building.
-
-mvn clean install -T8 -DskipTests -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
-
