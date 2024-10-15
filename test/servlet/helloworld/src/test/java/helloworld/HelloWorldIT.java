de3b63f37b04f33d0971af00b32d6eeaaf736e15
Manfred Riem <mriem@manorrock.com>
2024-10-15 16:53:48 -0500

    Fixes #4047 - Update plugins (#4051)

--- test/servlet/helloworld/src/test/java/helloworld/HelloWorldIT.java
+++ test/servlet/helloworld/src/test/java/helloworld/HelloWorldIT.java
@@ -9,12 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class HelloWorldIT {
- 
+
     @Test
     void testHelloWorldHtml() throws Exception {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest
-                .newBuilder(new URI("http://localhost:8080/helloworld/helloworld.html"))
+                .newBuilder(new URI("http://localhost:"
+                        + Integer.valueOf(System.getProperty("httpPort")) 
+                        + "/helloworld/helloworld.html"))
                 .build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         assertTrue(response.body().contains("Hello World!"));
