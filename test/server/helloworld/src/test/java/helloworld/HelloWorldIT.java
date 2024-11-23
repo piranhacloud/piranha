b607d0ba6d05c6782e0c44f8134793715abb1640
Manfred Riem <mriem@manorrock.com>
2024-11-23 12:15:10 -0600

    Fixes #4253 - Change test/server/helloworld module to use generate HTTP port

--- test/server/helloworld/src/test/java/helloworld/HelloWorldIT.java
+++ test/server/helloworld/src/test/java/helloworld/HelloWorldIT.java
@@ -39,7 +39,7 @@ class HelloWorldIT {
  
     @Test
     void testHelloWorldHtml() throws Exception {
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest
                 .newBuilder(new URI("http://localhost:" + System.getProperty("httpPort") + "/helloworld/helloworld.html"))
